6362b8abaf68f7a022681c1112a5271d2c10d4e6
Alli Sarmishta <22wh1a1213@bvrithyderabad.edu.in>
2025-11-02 12:19:29 +0530

    Initial commit: project backup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import {Mona_Sans} from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+
+
+
+const monaSans = Mona_Sans({
+    variable: "--font-mona-sans",
+    subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+    title: "PrepGenie",
+    description: "An AI powered platform for preparing for Mock-interviews",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" className="dark">
+            <body
+                className={`${monaSans.className} antialiased pattern`}
+            >
+            {children}
+            <Toaster/>
+        </body>
+        </html>
+    );
+}
